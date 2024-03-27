ea71adf79425807b55904cf0ade33e9601e8ab57
Cedric <cedsmit@gmail.com>
2024-03-27 12:04:35 +0100

    Added Base Project

---
+++ MainApp/MainApp/Page/CarPerformance.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Page;
+
+public partial class CarPerformance : ContentPage
+{
+    public CarPerformance()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainApp/MainApp/Page/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MainApp;
+
+public partial class MainPage : ContentPage
+{
+    
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnSubmitClicked(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}

38afeecc8dc6ffa0d399c780bd76cc61168cb1ef
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2024-04-01 18:22:26 +0200

    Fixed car data display not working

--- MainApp/MainApp/ViewModels/TuningViewModel.cs
+++ MainApp/MainApp/ViewModels/TuningViewModel.cs
@@ -5,39 +5,31 @@ using MainApp.Logic;
 
 namespace MainApp.ViewModels;
 
-public partial class TuningViewModel : ObservableObject
+public partial class TuningViewModel : ObservableObject, IQueryAttributable
 {
     [ObservableProperty] private string? _plate;
     [ObservableProperty] private CarData? _carData;
     private readonly IApiService _apiService = new ApiService();
 
     [RelayCommand]
-    private async Task SetCarData()
+    private async Task Submit(string licensePlate)
     {
-        var test = Plate;
-        CarData = await this._apiService.QueryRdwData(Plate);
-    }
-
-    [RelayCommand]
-    private async Task Submit()
-    {
-        await Shell.Current.GoToAsync($"TuningResultPage");
-        await SetCarData();
+        await Shell.Current.GoToAsync($"TuningResultPage?license={licensePlate}");
     }
     
-    // public void ApplyQueryAttributes(IDictionary<string, object> query)
-    // {
-    //     var licensePlate = query["license"] as string;
-    //     Plate = query["license"] as string;
-    //
-    //     if (licensePlate != null)
-    //     {
-    //         Task.Run(async () =>
-    //         {
-    //             await SetCarData(licensePlate);
-    //         }).GetAwaiter().GetResult();
-    //     }
-    //
-    //     Trace.WriteLine(licensePlate);
-    // }
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        var licensePlate = Plate = query["license"] as string;
+
+        // Create separate thread to prevent deadlock
+        if (licensePlate != null)
+        {
+            Task.Run(async () =>
+            {
+                CarData = await _apiService.QueryRdwData(licensePlate);
+            }).GetAwaiter().GetResult();
+        }
+
+        Trace.WriteLine(licensePlate);
+    }
 }
